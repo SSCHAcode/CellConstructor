Terrific bug on the diagonalization A superbug in the numpy diagonalization subroutines prevent the output eigenvectors from beeing normalized. Now I force the normalization of the polarization vector.

cellconstructor/Methods.py
@@ -510,6 +510,24 @@ def get_unit_cell_from_ibrav(ibrav, celldm):
         unit_cell[0, :] = np.array([1, 0, 0]) * a
         unit_cell[1, :] = np.array([-0.5, np.sqrt(3)/2, 0]) * a
         unit_cell[2, :] = np.array([0, 0, 1]) * c
+    elif ibrav == 9:
+        # Orthorombinc base centered
+        a = celldm[0] * BOHR_TO_ANGSTROM
+        b = celldm[1] * a
+        c = celldm[2] * a
+        
+        unit_cell[0, :] = np.array([a/2, b/2, 0])
+        unit_cell[1, :] = np.array([-a/2, b/2, 0])
+        unit_cell[2, :] = np.array([0, 0, c])
+    elif ibrav == -9:
+        # Orthorombinc base centered (the same but change the first two vectors)
+        a = celldm[0] * BOHR_TO_ANGSTROM
+        b = celldm[1] * a
+        c = celldm[2] * a
+        
+        unit_cell[0, :] = np.array([-a/2, b/2, 0])
+        unit_cell[1, :] = np.array([a/2, b/2, 0])
+        unit_cell[2, :] = np.array([0, 0, c])
     elif ibrav == 13:
         # Monoclinic base-centered
         
@@ -630,7 +648,11 @@ def read_namelist(line_list):
             variable = new_list[0].strip()
             value = new_list[1].strip()
             
-            # If it is a string cancel the " or '
+            # Remove ending comma and otehr tailoring space
+            if value[-1] == ",":
+                value = value[:-1].strip()
+            
+            # If it is a string cancel the " or ' or ,
             value = value.replace("\"", "").replace("'", "")
             
             # Convert fortran bool

cellconstructor/Phonons.py
@@ -349,7 +349,9 @@ class Phonons:
         The frequencies and polarization are ordered. Negative frequencies are to
         be interpreted as instabilities and imaginary frequency, as for QE.
         
-        They are returned 
+        They are returned.
+        
+        NOTE: The normalization is forced, as it is problematic for degenerate modes
         
         Parameters
         ----------
@@ -379,9 +381,9 @@ class Phonons:
 
         real_dyn *= self.dynmats[iq]
         
-        eigvals, pol_vects = np.linalg.eig(real_dyn)
+        eigvals, pol_vects = np.linalg.eigh(real_dyn)
         
-        f2 = np.real(eigvals)
+        f2 = eigvals
         
         # Check for imaginary frequencies (unstabilities) and return them as negative
         frequencies = np.zeros(len(f2))
@@ -392,6 +394,10 @@ class Phonons:
         sorting_mask = np.argsort(frequencies)
         frequencies = frequencies[sorting_mask]
         pol_vects = pol_vects[:, sorting_mask]
+        
+        # Force normalization
+        for i in range(3 * self.structure.N_atoms):
+            pol_vects[:, i] /= np.sqrt(pol_vects[:, i].dot(pol_vects[:, i]))
         
         return frequencies, pol_vects
     