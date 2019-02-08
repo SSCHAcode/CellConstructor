cdb620d6604dbbcfce370dd4da0ccba9e6d989e8
Lorenzo Monacelli <mesonepigreco@gmail.com>
2019-02-08 17:38:50 +0100

    Use the last test

--- tests/TestTwoBodyDOS/test_two_body_dos.py
+++ tests/TestTwoBodyDOS/test_two_body_dos.py
@@ -19,8 +19,8 @@ dyn.Symmetrize()
 w_array = linspace(0, 3500, 10000) / CC.Phonons.RY_TO_CM
 
 # Get the two body DOS at gamma
-Gamma = 10 / CC.Phonons.RY_TO_CM
-DOS = dyn.get_two_phonon_dos(w_array, Gamma, 100)
+Gamma = 3 / CC.Phonons.RY_TO_CM
+DOS = dyn.get_two_phonon_dos(w_array, Gamma, 50, exclude_acustic = True)
 
 DOS = abs(DOS)
 # Plot the results
