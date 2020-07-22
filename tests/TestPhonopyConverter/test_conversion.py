82df68277a322bbc192aba2df90c1a86b17abf3d
Lorenzo Monacelli <mesonepigreco@gmail.com>
2020-07-22 19:10:00 +0200

    Update test_conversion.py
    Modified the test for the phonopy conversion so that even if it fails, the whole test suite will be executed with a complete report on the failed tests

--- tests/TestPhonopyConverter/test_conversion.py
+++ tests/TestPhonopyConverter/test_conversion.py
@@ -11,21 +11,26 @@ import cellconstructor.Phonons
 import numpy as np
 import os
 
+def test_phonopy_converter():
+    total_path = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(total_path)
 
-total_path = os.path.dirname(os.path.abspath(__file__))
-os.chdir(total_path)
-                             
-# Read the phonons (4 irreducible q points)
-dynmat = CC.Phonons.Phonons("unitcell.in",use_Phonopy=True, nqirr=4)
+    # Read the phonons (4 irreducible q points)
+    dynmat = CC.Phonons.Phonons("unitcell.in",use_Phonopy=True, nqirr=4)
 
-# Write all the q points and compute the frequency in Gamma
-print ("The q points are:")
-print (dynmat.q_tot)
-print ()
-freq, pol_vect = dynmat.DyagDinQ(0)
-print ("Frequencies in Gamma:")
-print (np.sort(freq))
+    # Write all the q points and compute the frequency in Gamma
+    print ("The q points are:")
+    print (dynmat.q_tot)
+    print ()
+    freq, pol_vect = dynmat.DyagDinQ(0)
+    print ("Frequencies in Gamma:")
+    print (np.sort(freq))
 
 
-# Test the saving method
-dynmat.save_qe("QE_dyn")
+    # Test the saving method
+    dynmat.save_qe("QE_dyn")
+
+    
+if __name__ == "__main__":
+    test_phonopy_converter()
+  
