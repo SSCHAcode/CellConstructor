2f9d261ad3d2a653340c2c6253e8812b95750469
diegomartinez2 <diegomartinez2@yahoo.es>
2022-06-30 15:30:24 +0200

    Added a recursive function in case the number of vibrational eigenmodes is bigger than the limit of the Sobol generator.

--- cellconstructor/Moro_object.py
+++ cellconstructor/Moro_object.py
@@ -119,6 +119,18 @@ class Moro(object):
         #print (x)
 
         return x
+
+    def Sobol_big(self,size,n_modes,scramble): # in case the number of vibrational modes excedes the max setting of the generator.
+        if (n_modes>21201):
+            number = n_modes-21201
+            #if (number>21201): #Do it recursive???
+            x1 = sobol_modes(size = size, n_modes = 21201 , scramble = scramble)
+            x2 = sobol_big(size = size, n_modes = number, scramble = scramble)
+            x = x1.append(x2)
+        else:
+            x = sobol_modes(size = size, n_modes = n_modes , scramble = scramble)
+        return x
+        
 # ----------
 # Funciones
 # ----------
