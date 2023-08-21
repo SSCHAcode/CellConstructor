d74b24c08794093534a220ad6a2bd5df2f8ae114
diegomartinez2 <diegomartinez2@yahoo.es>
2023-08-21 12:58:29 +0200

    Not a fix?

--- cellconstructor/Spectral.py
+++ cellconstructor/Spectral.py
@@ -242,10 +242,7 @@ def get_static_correction_interpolated(dyn, tensor3, T, new_supercell, k_grid):
 
     # Add all the new computed dynamical matrix
     for iq in range(len(q_tot)):
-        if (iq==0):
-            new_dyn.dynmats[iq] = dynq[iq, :, :]
-        else:
-            new_dyn.dynmats.append(dynq[iq, :, :])
+        new_dyn.dynmats.append(dynq[iq, :, :])
 
     # Adjust the dynamical matrix q points and the stars
     new_dyn.AdjustQStar()
